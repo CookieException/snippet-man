f7ad5969c62fd1f013f872e88db80eca24779814
Nockiro <info@nockiro.de>
2019-11-03 15:19:28 +0100

    - added interface for database connections

--- SnippetMan/SnippetMan/Classes/Snippets/SnippetCode.cs
+++ SnippetMan/SnippetMan/Classes/Snippets/SnippetCode.cs
@@ -2,18 +2,15 @@
 {
     public class SnippetCode
     {
-        public int Id { get; set; }
         public string Imports { get; set; }
         public string Code { get; set; }
 
-        public SnippetCode(int id)
+        public SnippetCode()
         {
-            Id = id;
         }
 
-        public SnippetCode(int id, string imports, string code)
+        public SnippetCode(string imports, string code)
         {
-            Id = id;
             Imports = imports;
             Code = code;
         }
