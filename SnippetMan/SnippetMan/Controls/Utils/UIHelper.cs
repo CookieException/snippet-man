e8039eba7d3ade590805c3e9f9071993b0b6e272
Nockiro <info@nockiro.de>
2019-11-13 00:22:20 +0100

    Restliche Controls in SnippetPage ergänzt und dortige Styles angewendet/umgebaut

--- SnippetMan/SnippetMan/Controls/Utils/UIHelper.cs
+++ SnippetMan/SnippetMan/Controls/Utils/UIHelper.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 
 namespace SnippetMan.Controls.Utils
 {
@@ -20,6 +22,23 @@ namespace SnippetMan.Controls.Utils
                     return resultChildren;
             }
             return null;
+        }
+    }
+    public class MaxHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double pctHeight = (double)parameter;
+
+            if ((pctHeight <= 0.0) || (pctHeight > 100.0))
+                throw new Exception("MaxHeightConverter expects parameter in the range (0,100]");
+
+            return ((double)value * pctHeight);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
