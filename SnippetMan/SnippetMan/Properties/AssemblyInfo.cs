952648083c547622b93b0aebe9c97b7d8c6a0c4c
Nockiro <info@nockiro.de>
2020-01-29 22:44:20 +0100

    Einzige deutsche Zeichenkette auf Englisch geändert / Assembly Informationen angepasst

--- SnippetMan/SnippetMan/Properties/AssemblyInfo.cs
+++ SnippetMan/SnippetMan/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("SnippetMan")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A code snippet management tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SnippetMan")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
